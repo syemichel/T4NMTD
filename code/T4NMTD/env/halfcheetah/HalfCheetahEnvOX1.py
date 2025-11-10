9c0ed2254bc1eff94916d5b927fd7ce8e9a88307
syemichel <60457772+syemichel@users.noreply.github.com>
2025-11-10 15:55:58 +0800

    Add files via upload

---
+++ code/T4NMTD/env/halfcheetah/HalfCheetahEnvOX1.py
@@ -0,0 +1,213 @@
+import random
+import time
+
+import ray
+
+from util.utils import *
+from gymnasium import utils
+from gymnasium.envs.mujoco import MujocoEnv
+from gymnasium.spaces import Box, Dict, Discrete
+
+dfa_text1 = '''if(ds==@q1 ^ (~p2 & ~p4)) then @q1
+else if(ds==@q1 ^ (p4)) then @q3
+else if(ds==@q1 ^ (p2 & ~p4)) then @q2
+else if(ds==@q3 ^ (~p3 & ~p5)) then @q3
+else if(ds==@q3 ^ (p5 & ~p3)) then @q2
+else if(ds==@q3 ^ (p3)) then @q4
+else if(ds==@q2 ^ (True)) then @q2
+else if(ds==@q4 ^ (~p2 & ~p4)) then @q4
+else if(ds==@q4 ^ (p4 & ~p2)) then @q2
+else if(ds==@q4 ^ (p2)) then @q5
+else if(ds==@q5 ^ (True)) then @q5'''
+
+DEFAULT_CAMERA_CONFIG = {
+    "distance": 4.0,
+}
+
+class DFATransformer:
+    def __init__(self, dfa_text):
+        self.dfa_text = dfa_text1
+        self.dfa = get_dfa(self.dfa_text)
+        self.dfa_state = '@q1'
+        self.accepting_state = "@q" + str(self.dfa.number_of_nodes())
+        self.error_state = "@q2"
+
+    def reset(self):
+        self.dfa_state = '@q1'
+
+    def evaluate_logic_formula(self, props:Dict, formula):
+        # 创建一个字典，将命题名称映射到它们的值
+        '''props = {'p1': p1,'p2': p2,'p3': p3}'''
+        # 替换公式中的命题名称为对应的布尔值
+
+        for var, value in props.items():
+            formula = re.sub(r'\b' + re.escape(var) + r'\b', str(value), formula)
+        formula = formula.replace('~', 'not ').replace('&', 'and ').replace('|', 'or ')
+        # 评估公式
+        try:
+            result = eval(formula)
+            return result
+        except Exception as e:
+            print(f"评估公式时出错: {e}")
+            return None
+
+    # return terminate, if_success and if_failure
+    def step(self, props):
+        out_edges = self.dfa.out_edges(str(self.dfa_state), data=True)
+        for edge in out_edges:
+            if self.evaluate_logic_formula(props, formula=edge[2]['formula']):
+                self.dfa_state = edge[1]
+                break
+        if self.dfa_state == self.error_state:
+            return True, False, True
+        if self.dfa_state == self.accepting_state:
+            return True, True, False
+        return False, False, False
+
+
+class HalfCheetahEnvOX1(MujocoEnv, utils.EzPickle):
+    metadata = {
+        "render_modes": [
+            "human",
+            "rgb_array",
+            "depth_array",
+        ],
+        "render_fps": 20,
+    }
+
+    def __init__(
+        self,
+        option_index,
+        reset_ps,
+        forward_reward_weight=1.0,
+        ctrl_cost_weight=0.1,
+        reset_noise_scale=0,
+        exclude_current_positions_from_observation=False,
+        dfa_text=None,
+    ):
+        utils.EzPickle.__init__(
+            self,
+            option_index,
+            reset_ps,
+            forward_reward_weight,
+            ctrl_cost_weight,
+            reset_noise_scale,
+            exclude_current_positions_from_observation,
+        )
+
+        self.currentH = 0
+        self.points = {'p1': -8, 'p2': -4, 'p3': 0, 'p4': 4, 'p5': 8}
+        self.horizon = 1000
+        # initialize dfa
+        self.dfa = DFATransformer(dfa_text1)
+        self.automata_states_num = self.dfa.dfa.number_of_nodes()
+        self.option_index = option_index
+        self.start = False
+        self.reset_ps = reset_ps
+        self.end_states = None
+        self.buffer_action = None
+        self.initial_state = None
+        self.value = None
+        self.log_prob = None
+
+        self._forward_reward_weight = forward_reward_weight
+        self._ctrl_cost_weight = ctrl_cost_weight
+        self._reset_noise_scale = reset_noise_scale
+        self._exclude_current_positions_from_observation = (
+            exclude_current_positions_from_observation
+        )
+
+        observation_space = Box(
+            low=-np.inf, high=np.inf, shape=(18,), dtype=np.float64
+        )
+
+        MujocoEnv.__init__(
+            self,
+            "half_cheetah.xml",
+            5,
+            observation_space=observation_space,
+            default_camera_config=DEFAULT_CAMERA_CONFIG,
+        )
+
+    def control_cost(self, action):
+        control_cost = self._ctrl_cost_weight * np.sum(np.square(action))
+        return control_cost
+
+    def step(self, action):
+        x_position_before = self.data.qpos[0]
+        self.do_simulation(action, self.frame_skip)
+        x_position_after = self.data.qpos[0]
+        x_velocity = -(x_position_after - x_position_before) / self.dt
+        ctrl_cost = self.control_cost(action)
+        forward_reward = self._forward_reward_weight * x_velocity
+        observation = self._get_obs()
+
+        # get props
+        props = {'p1': False, 'p2': False, 'p3': False, 'p4': False, 'p5': False}
+        for k, v in self.points.items():
+            if abs(v - x_position_after) < 0.3:
+                props[k] = True
+                break
+        last_dfa_state = int(self.dfa.dfa_state.split('q')[1])
+        terminated, if_success, if_failure = self.dfa.step(props)
+        dfa_state = int(self.dfa.dfa_state.split('q')[1])
+
+        # reward shaping
+        if if_success:
+            reward = 100
+        else:
+            reward = 0
+
+        info = {
+            "x_position": x_position_after,
+            "x_velocity": x_velocity,
+            "reward_run": forward_reward,
+            "reward_ctrl": -ctrl_cost,
+            'is_success': if_success
+        }
+
+        if self.render_mode == "human":
+            self.render()
+
+        self.currentH += 1
+        truncated = False
+        if self.currentH == self.horizon:
+            truncated = True
+
+        return observation, reward, terminated, truncated, info
+
+    def _get_obs(self):
+        position = self.data.qpos.flat.copy()
+        velocity = self.data.qvel.flat.copy()
+
+        if self._exclude_current_positions_from_observation:
+            position = position[1:]
+
+        # calculate distance from each subgoal
+        observation = np.concatenate((position, velocity)).ravel()
+        return observation
+
+    def reset_model(self):
+        assert self.reset_ps is not None
+        reset_state = ray.get(self.reset_ps.get_state.remote(self.option_index))
+        while not self.start:
+            if reset_state is None:
+                time.sleep(5)
+                reset_state = ray.get(self.reset_ps.get_state.remote(self.option_index))
+            else:
+                self.start = True
+                print(self.option_index, "start!!!")
+                break
+        info = reset_state
+        obs, self.end_states = info
+        self.initial_state = obs
+        self.currentH = 0
+        original_obs = obs['obs']
+        self.dfa.dfa_state = '@q' + str(obs['ds'].item() + 1)
+        self.set_state(original_obs[0][0][0:9], original_obs[0][0][9:18])
+        observation = self._get_obs()
+
+        return observation
+
+
+
